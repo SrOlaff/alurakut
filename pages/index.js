3749ee2aba426ad2ebffa7ba24ed172c7861a16e
SrOlaff <polavo00@gmail.com>
2021-07-19 13:48:17 -0300

    Mundaça Link Fetch API

--- pages/index.js
+++ pages/index.js
@@ -207,7 +207,7 @@ export default function Home(props) {
 export async function getServerSideProps(context) {
   const cookies = nookies.get(context);
   const token = cookies.USER_TOKEN;
-  const { githubUser } = jwt.decode(token);
+  const { githubUser } = jwt.decode(token) ? jwt.decode(token) : "";
   const { isAuthenticated } = await fetch(
     "https://alurakut-mmewsdvis-srolaff.vercel.app/api/auth",
     {
